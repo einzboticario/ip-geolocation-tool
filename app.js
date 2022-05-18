65b67a4668e6e4e803e76001b4a519c765f77a8b
Einz Luis <einzluis@gmail.com>
2022-05-19 01:47:32 +0800

    display user ip when page loads up

---
+++ app.js
@@ -0,0 +1,13 @@
+//display user ip
+var user_ip = document.getElementById("user_ip");
+
+fetch('https://api.ipify.org/?format=json')
+    .then(results => results.json())
+    .then(data => user_ip.innerHTML = data.ip);
+
+$(document).ready(function(){
+    $("#form").submit(function(event){
+        event.preventDefault;
+
+    })
+})
