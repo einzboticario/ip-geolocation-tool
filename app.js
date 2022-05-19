7f6e5af377062ede19109719c2f459735186496b
Einz Luis Boticario <einzluisboticario@gmail.com>
2022-05-20 02:14:56 +0800

    can now request data from API

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 //display user ip
-var user_ip = document.getElementById("user_ip");
+const user_ip = document.getElementById("user_ip");
 
 fetch('https://api.ipify.org/?format=json')
     .then(results => results.json())
     .then(data => user_ip.innerHTML = data.ip);
 
 $(document).ready(function(){
+
+    //ip search submit
     $("#form").submit(function(event){
-        event.preventDefault;
+
+        event.preventDefault();
+        let ip = $("#input_ip").val();
+
+        //if submitted blank, alert
+        if(ip == ""){
+            alert("Please enter an IP address.");
+        }
+
+        const API_KEY = "1d9fa633bb0e412393c98f0ad011fd88";
+        let url = "https://api.ipgeolocation.io/ipgeo?apiKey="+API_KEY+"&ip="+ip;
+
+        $.get(url, function(data){
+            //show data in console
+            console.log(data);
+        })
+        // zzz
     })
 })
