2086c402f5ac55817fd1f2a1271f074e1db10d15
Einz Luis Boticario <einzluisboticario@gmail.com>
2022-05-21 14:27:39 +0800

    get latitude and longitude from api

--- app.js
+++ app.js
@@ -30,25 +30,35 @@ $(document).ready(function(){
             //show data in console
             console.log(data);
 
+            //it's readable
             var country_flag = data.country_flag;
             var city = data.city;
             var country_name = data.country_name;
 
+            //get latitude and longitude, convert it from string to float
+            var lat = parseFloat(data.latitude);
+            var lng = parseFloat(data.longitude);
+
+            //will use this data to show location in google map
+            var pos = {
+                lat: lat,
+                lng: lng
+            }
+
             displayDetails(country_flag, city, country_name);
 
-            //test
-            console.log(country_flag);
-            console.log(city);
-            console.log(country_name);
+            //show pos in console
+            console.log(pos)
         })
     })
+    
 
+    //Functions
     function displayDetails(country_flag, city, country_name) {
-        
         //append html containing ip details
-        var ip_details = `<h4>${city}, ${country_name}</h4><br> 
-                        <img src=${country_flag}`;
-
+        var ip_details = `<img src=${country_flag}><h4>${city}, ${country_name}</h4>`;
+        
         $("#ip_details").append(ip_details);
     }
+
 })
