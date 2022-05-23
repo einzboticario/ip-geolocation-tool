8a1ff19df4da0a8cbd514cd291968722ee372266
Einz Luis Boticario <einzluisboticario@gmail.com>
2022-05-23 19:46:20 +0800

    append data on 'ip_details' and 'others'

--- app.js
+++ app.js
@@ -1,81 +1,70 @@
-
-//display user ip
+// display user ip
 const user_ip = document.getElementById("user_ip");
 fetch('https://api.ipify.org/?format=json')
     .then(results => results.json())
     .then(data => user_ip.innerHTML = data.ip);
 
-//google map
-function initMap(){
-
-    //default loc
-    const def = { lat: 14.5589, lng: 121.0181 };
-
-    var options = {
-        zoom: 15,
-        center: def,
-    }
-
-    //map centered at default loc
-    const map = new google.maps.Map(document.getElementById('map'), {options});
-
-    //marker placed at default loc
-    const marker = new google.maps.Marker({
-        position: def,
-        map: map,
-    })
-}
-
 $(document).ready(function(){
-    //ip search submit
+    // ip search submit
     $("#form").submit(function(event){
 
         event.preventDefault();
         const ip = $("#input_ip").val();
 
-        //clear ip_details div when submitted
+        // empty these when submitted
         $("#ip_details").empty();
+        $("others").empty();
 
-        //if submitted blank, alert
+        // if submitted blank, alert
         if(ip == ""){
             alert("Please enter an IP address.");
         }
 
-        //ipgeoloc api
+        // ipgeoloc api
         var API_KEY = "1d9fa633bb0e412393c98f0ad011fd88";
         var url = `https://api.ipgeolocation.io/ipgeo?apiKey=${API_KEY}&ip=${ip}`;
 
         $.get(url, function(data){
-            //show data in console
+            // show data in console
             console.log(data);
 
-            var country_flag = data.country_flag;
-            var city = data.city;
             var country_name = data.country_name;
+            var city = data.city;
+            var zipcode = data.zipcode;
+            var isp = data.isp;
+            var country_flag = data.country_flag;
 
-            //get latitude and longitude, convert it from string to float
-            var lat = parseFloat(data.latitude);
-            var lng = parseFloat(data.longitude);
+            // get latitude and longitude, convert it from string to float
+            var latitude = parseFloat(data.latitude);
+            var longitude = parseFloat(data.longitude);
 
-            //will use this data to show location in google map
-            var pos = {
-                lat: lat,
-                lng: lng
-            }
-
-            displayDetails(country_flag, city, country_name);
-
-            //show pos in console
-            console.log(pos)
+            displayDetails(country_name, city, zipcode, isp, latitude, longitude, country_flag);   
         })
     })
 
-    //functions
-    function displayDetails(country_flag, city, country_name) {
-        //append html containing ip details
-        var ip_details = `<img src=${country_flag}><h4>${city}, ${country_name}</h4>`;
-        
+    // function
+
+    function displayDetails(country_name, city, zipcode, isp, latitude, longitude, country_flag) {
+
+        var ip_details = `
+        <h3>Country Name: <span class="ip_data">${country_name}</span></h2>
+        <h3>City: <span class="ip_data">${city}</span></h2>
+        <h3>Zip Code: <span class="ip_data">${zipcode}</span></h2>
+        <h3>ISP: <span class="ip_data">${isp}</span></h2>
+        <h3>Latitude: <span class="ip_data">${latitude}</span></h2>
+        <h3>Longitude: <span class="ip_data">${longitude}</span></h2>
+        `
+
+        var other_details = `
+        <img src="${country_flag}" alt="">
+        <button>See google map</button>
+        `
+
         $("#ip_details").append(ip_details);
+        $("#others").append(other_details);
     }
+
+    // to fix: 
+    // stacking img and button elements 
 
 })
